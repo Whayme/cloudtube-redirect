aa839d6e24619bddc61a5bf71f648dbb161a1c01
Scarlett <63085504+Whayme@users.noreply.github.com>
2020-08-05 12:49:51 -0500

    Update makehook.js

--- makehook.js
+++ makehook.js
@@ -1,16 +1,16 @@
 var loc = window.location.href;
 var newloc = loc;
 if(newloc.includes("m.youtube.com")){
-        newloc=newloc.replace("m.youtube.com", "invidio.us");
+        newloc=newloc.replace("m.youtube.com", "cadence.moe");
 }
 else if (newloc.includes("youtube")){
-	newloc=newloc.replace("youtube.com", "invidio.us");
+	newloc=newloc.replace("youtube.com", "cadence.moe");
 }
 else if (newloc.includes("youtu.be")){
-	newloc=newloc.replace("youtu.be/", "invidio.us/watch?v=");
+	newloc=newloc.replace("youtu.be/", "https://cadence.moe/cloudtube/");
 }
 else if (newloc.includes("youtube-nocookie")){
-	newloc=newloc.replace("youtube-nocookie.com/", "invidio.us/");
+	newloc=newloc.replace("youtube-nocookie.com/", "cadence.moe/");
 }
 if(newloc.includes("/results?q")){
         newloc=newloc.replace("/results?q", "/search?q");
